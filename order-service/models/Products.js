07d707cd8e4f2913700c907344b403a1d3d9fe24
Peter Wu <peter.wu@techworkslabs.com>
2025-06-01 00:03:59 +0800

    corrected typo attribute name

--- order-service/models/Products.js
+++ order-service/models/Products.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
-    proudctId: {
+    productId: {
         type: String,
         required: true,
         unique: true,
